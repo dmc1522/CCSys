c63fbe63ff23a39e823257a3fbb777f6d4a76b0e
nn <nn@MANUEL>
2016-10-22 04:02:17 -0500

    added POC for step 1 for add weighticket

--- LasMargaritas.UI/MainWindow.xaml.cs
+++ LasMargaritas.UI/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using LasMargaritas.BL;
+using LasMargaritas.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +24,23 @@ namespace LasMargaritas.UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string baseUrl;
+        private string insertAction;
+        private string updateAction;
+        private string deleteAction;
+        private string getAllAction;
+        private string getByIdAction;
+       
+
         public MainWindow()
         {
             InitializeComponent();
+            baseUrl = @"http://lasmargaritas.azurewebsites.net/";
+            insertAction = "Producer/Add";
+            updateAction = "Producer/Update";
+            deleteAction = "Producer/Delete";
+            getAllAction = "Producer/GetAll";
+            getByIdAction = "Producer/GetById";
         }
 
         private void ProductsBorder_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -34,31 +52,50 @@ namespace LasMargaritas.UI
         {
             Dispatcher.Invoke(new Action(() => 
             {
-                SetView(View.WeighTicketsMain);
+                SetView(View.WeightTicketsMain);
             }));
         }
         private enum View
         {
             Main,
-            WeighTicketsMain
+            WeightTicketsMain,
+            WeightTicketsCreateStep1
         }
         private void SetView(View view)
         {
             switch(view)
             {
                 case View.Main:
-                    MainPage.Visibility = Visibility.Visible;
-                    WeightTicketsMain.Visibility = Visibility.Collapsed;
+                    MainView.Visibility = Visibility.Visible;
+                    WeightTicketsMainView.Visibility = Visibility.Collapsed;
+                    WeighTicketsCreateStep1View.Visibility = Visibility.Collapsed;
                     break;
-                case View.WeighTicketsMain:
-                    MainPage.Visibility = Visibility.Collapsed;
-                    WeightTicketsMain.Visibility = Visibility.Visible;                   
+                case View.WeightTicketsMain:
+                    MainView.Visibility = Visibility.Collapsed;
+                    WeightTicketsMainView.Visibility = Visibility.Visible;
+                    WeighTicketsCreateStep1View.Visibility = Visibility.Collapsed;
+                    break;
+                case View.WeightTicketsCreateStep1:
+                    MainView.Visibility = Visibility.Collapsed;
+                    WeightTicketsMainView.Visibility = Visibility.Collapsed;
+                    WeighTicketsCreateStep1View.Visibility = Visibility.Visible;
                     break;
             }
 
         }
+      
 
-        private void BorderWeightTicketsMainGoBack_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+       
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            Dispatcher.Invoke(new Action(() =>
+            {
+                SetView(View.Main);
+            }));
+        }
+
+        private void GoBackFromWeightTicketsMainBorder_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             Dispatcher.Invoke(new Action(() =>
             {
@@ -66,12 +103,55 @@ namespace LasMargaritas.UI
             }));
         }
 
-        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+
+        private void GoBackFromWeightTicketCreateStep1Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             Dispatcher.Invoke(new Action(() =>
             {
-                SetView(View.Main);
+                SetView(View.WeightTicketsMain);
             }));
+        }
+       
+        private void WeightTicketsCreateBorder_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Dispatcher.Invoke(new Action(() =>
+            {
+                SetView(View.WeightTicketsCreateStep1);
+            }));
+
+        }
+
+     
+
+        private void TextBoxProducerId_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {                
+                Token token = TokenHelper.GetToken(baseUrl, "Melvin3", "MelvinPass3");
+                string getByIdUrl = string.Format("{0}?id={1}", getByIdAction, TextBoxProducerId.Text);
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.access_token);
+                HttpResponseMessage response = client.GetAsync(getByIdUrl).Result;
+                if (response.IsSuccessStatusCode)
+                {                 
+                    GetProducerResponse getProducerResponse = response.Content.ReadAsAsync<GetProducerResponse>().Result;
+                    if (getProducerResponse.Producers.Count == 1)
+                    {
+                        GridProducerData.Visibility = Visibility.Visible;
+                        LabelProducerName.Content = getProducerResponse.Producers[0].Name;
+                        LabelProducerLastName.Content = getProducerResponse.Producers[0].LastName;
+                        LabelProducerAddress.Content = getProducerResponse.Producers[0].Address;
+                        LabelProducerCity.Content = getProducerResponse.Producers[0].City;
+                    }
+                }
+            }
+            else
+            {
+                GridProducerData.Visibility = Visibility.Hidden;
+            }
         }
     }
 }
+
---
+++ LasMargaritas.UT/BLandDL/ProducerBLDLTest.cs
@@ -0,0 +1,38 @@
+﻿using LasMargaritas.BL;
+using LasMargaritas.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+namespace LasMargaritas.ULT
+{
+    [TestClass]
+    public class ProducerBLDLTest
+    {
+        private string connectionString;
+        public ProducerBLDLTest()
+        {
+            connectionString = "Server=tcp:lasmargaritas.database.windows.net,1433;Initial Catalog=LasMargaritasDB;Persist Security Info=False;User ID=LasMargaritasDbUser;Password=LasMargaritasPassw0rd!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            if (ConfigurationManager.ConnectionStrings["LasMargaritasDb"] != null)
+            {
+                connectionString = ConfigurationManager.ConnectionStrings["LasMargaritasDb"].ConnectionString;
+            }
+            connectionString = ConfigurationManager.ConnectionStrings["LasMargaritasDb"].ConnectionString;
+        }
+        
+        [TestMethod]
+        public void TestInsertUpdateAndGetWeightTicket()
+        {
+            //Test get by Id
+            ProducerBL producerBL = new ProducerBL(connectionString);
+            List<Producer> producers = producerBL.GetProducer(2);
+            Assert.IsTrue(producers.Count() == 1);
+            Producer producer = producers[0];
+            //Assert.IsTrue(producer.Id == weightTicket.Id); //Todo create custom validator to check all properties
+         
+        }
+
+      
+    }
+}
