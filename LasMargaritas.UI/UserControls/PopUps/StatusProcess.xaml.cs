337680f2e9090a66c7f4cf0b5f859231d5ef2cc2
Xavier Medina <Xavier Medina@DESKTOP-E0L7BK8>
2016-11-23 10:24:19 -0600

    Fix Styles

---
+++ LasMargaritas.UI/UserControls/PopUps/StatusProcess.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LasMargaritas.UI.UserControls.PopUps
+{
+    /// <summary>
+    /// Interaction logic for StatusProcess.xaml
+    /// </summary>
+    public partial class StatusProcess : UserControl
+    {
+        public StatusProcess()
+        {
+            InitializeComponent();
+            switch (TypeProcess)
+            {
+                case "1":
+                    Console.WriteLine("Case 1");
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+
+            if(IsSuccess)
+                btnCicle.Visibility= Visibility.Hidden;
+        }
+
+        public string TypeProcess
+        {
+            get { return (string)GetValue(TypeProcessProperty); }
+            set { SetValue(TypeProcessProperty, value); }
+        }
+
+        public bool IsSuccess
+        {
+            get { return (bool)GetValue(IsSuccessProperty); }
+            set { SetValue(IsSuccessProperty, value); }
+        }
+
+        public static readonly DependencyProperty TypeProcessProperty = DependencyProperty.Register("TypeProcess", typeof(string), typeof(string));
+        public static readonly DependencyProperty IsSuccessProperty = DependencyProperty.Register("IsSuccess", typeof(bool), typeof(bool));
+
+
+    }
+}
